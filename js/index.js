d5c1b9c34d042b2f48384cd3e94502762537f03e
Abisena <abisena27@gmail.com>
2025-08-04 21:16:15 +0700

    feat(main): added toggle button, added pricing section. added typedjs feature in the hero section.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+//Typed Effect
+var typed = new Typed('#hero-p', {
+      strings: ['Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit dolorem quisquam ad, corporis laboriosam voluptate earum delectus rem iure incidunt, sunt tenetur laborum exercitationem qui.'],
+      typeSpeed: 0,
+      showCursor: false,
+      loop: true,
+    });
+
 // NAVBAR SCROLL EFFECT
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
@@ -21,8 +29,16 @@ function infiniteCarousel () {
     slidesPerView: 'auto',
     stopOnLastSlide: false,
     spaceBetween: -50,
+    allowTouchMove: false
   });
-}
+} infiniteCarousel();
 
-infiniteCarousel();
-
+function togglePrice() {
+  const button = document.querySelector('.button');
+  const circle = document.querySelector('.button-circle');
+  
+  button.addEventListener('click', () => {
+    circle.classList.toggle('toggle');
+    button.classList.toggle('toggleBG');
+  });
+} togglePrice();
